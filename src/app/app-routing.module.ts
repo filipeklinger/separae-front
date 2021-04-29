02b3d4036b27655b9f4650a192ba151b9061061c
Vinícius Martins <vinimartinsc@gmail.com>
2021-04-29 00:44:47 -0300

    add activity template e correção de add-hall

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import {DashboardComponent} from './components/dashboard/dashboard.component';
 import {AddHallComponent} from './components/add-hall/add-hall.component';
 import {SimulationComponent} from './components/simulation/simulation.component';
 import {RegisterComponent} from './components/register/register.component';
+import {AddActivityComponent} from './components/add-activity/add-activity.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'addHall', component: AddHallComponent},
+  {path: 'addActivity', component: AddActivityComponent},
   {path: 'simulation', component: SimulationComponent},
   {path: 'register', component: RegisterComponent},
   {path: '', redirectTo: 'main', pathMatch: 'full'}
