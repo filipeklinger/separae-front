1b64e74299ef732413d05ecb9e8ba9770bb49ef8
Vinícius Martins <vinimartinsc@gmail.com>
2021-04-29 10:01:38 -0300

    services

--- src/app/components/simulation/simulation.component.ts
+++ src/app/components/simulation/simulation.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {CalculationService} from '../../services/calculation.service';
+import {ActivityService} from '../../services/activity.service';
+import {TokenStorageService} from '../../services/token-storage.service';
 
 @Component({
   selector: 'app-simulation',
@@ -7,9 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SimulationComponent implements OnInit {
 
-  constructor() { }
+  form = {
+    width: null,
+    length: null,
+    activityId: '',
+    currentLevel: ''
+  };
+  userId: any = '';
+  isSuccessful = false;
+  errorMessage = '';
+  activityList = [];
+  calculated = {};
+
+  constructor(
+    private calculate: CalculationService,
+    private actList: ActivityService,
+    private router: Router,
+    private userData: TokenStorageService
+  ) { }
 
   ngOnInit(): void {
+    this.userId = this.userData.getToken();
+
+    this.actList.getActivityList().subscribe(
+      data => {
+        this.activityList = data.data;
+        console.log(data.data);
+      },
+      err => {
+        this.errorMessage = err.error.message;
+      }
+    );
   }
 
+  onSubmit(): void {
+    const {} = this.form;
+    console.log(this.form);
+
+    this.calculate.addHall(this.form).subscribe(
+      data => {
+        this.isSuccessful = true;
+        this.calculated = data;
+        console.log(data);
+        },
+      err => {
+        this.errorMessage = err.error.message;
+      }
+    );
+  }
+
+  redirect(): void{
+    if ( this.isSuccessful ) {
+      this.router.navigate(['login']);
+    }
+  }
 }
--- src/app/services/calculation.service.ts
+++ src/app/services/calculation.service.ts
@@ -3,7 +3,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 // commons
-const hallRoute = 'https://localhost:5001/hall/';
+const hallRoute = 'https://localhost:5001/Hall/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json-patch+json' })
@@ -17,7 +17,7 @@ export class CalculationService {
 
   constructor(private http: HttpClient) { }
 
-  addHall(hall: {name: string, width: number, length: number, activityId: string}): Observable<any>{
-    return this.http.post(hallRoute + 'calculateDistance', hall , httpOptions);
+  addHall(hall: any): Observable<any>{
+    return this.http.post(hallRoute + 'CalculateDistance', hall , httpOptions);
   }
 }
