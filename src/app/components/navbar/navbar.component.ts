ddb6fa3ecca5c7794ec8bdc47f0d2f8102c30bc1
Vinícius Martins <vinimartinsc@gmail.com>
2021-04-29 03:51:08 -0300

    navbar orientada a contexto logado ou não

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {TokenStorageService} from '../../services/token-storage.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  // flags
+  isLoggedIn = false;
+
+  constructor(
+    private authService: AuthService,
+    private tokenStorage: TokenStorageService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+    }
   }
 
+  logout(): void{
+    window.sessionStorage.clear();
+    this.router.navigate(['/home']);
+  }
 }
